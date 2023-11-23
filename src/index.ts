28be1850509809a726b5d1fcbc9c0f5d3e4763c2
CDN18 <cardinal@codeword.info>
2023-11-23 20:25:56 +0800

    feat: handle reblog, spoiler, reply and medias

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import fetch from 'node-fetch';
 import { Feed } from 'feed';
 
 export interface Env {
@@ -33,10 +32,27 @@ interface Status {
 	created_at: string;
 	url: string;
 	content: string;
+    reblog: Status;
+    spoiler_text: string;
+    in_reply_to_id: string;
+    media_attachments: MediaAttachment[];
 	account: {
 	  display_name: string;
 	  acct: string;
 	};
+}
+
+interface MediaAttachment {
+    type: string;
+    url: string;
+}
+
+interface FeedItem {
+    title: string;
+    id: string;
+    link: string;
+    content: string;
+    date: Date;
 }
 
 export default {
@@ -61,18 +77,93 @@ export default {
 			copyright: 'Powered by Fedi Timeline RSS Worker, created by Owu One. All rights reserved to the original author of the statuses in this feed.',
         });
 
-        statuses.forEach((status: Status) => {
-            feed.addItem({
-                title: `${status.account.display_name} (${status.account.acct})`,
-                id: status.id,
-                link: status.url,
-                content: status.content,
-                date: new Date(status.created_at),
-            });
-        });
+        for (const status of statuses) {
+            const item = await statusToItem(status, instance_url, access_token);
+            feed.addItem(item);
+        }
+        feed.items.sort((a, b) => b.date.getTime() - a.date.getTime());
 
         return new Response(feed.rss2(), {
             headers: { 'Content-Type': 'application/rss+xml' },
         });
     },
 };
+
+async function getStatus(instance_url: string, access_token: string, id: string): Promise<Status> {
+    const endpoint = `https://${instance_url}/api/v1/statuses/${id}`;
+    const headers = { 'Authorization': `Bearer ${access_token}` };
+
+    const response = await fetch(endpoint, { headers });
+    if (!response.ok) {
+        return {} as Status;
+    }
+
+    return await response.json() as Status;
+}
+
+function getName(status: Status): string {
+    if (status.account.display_name) {
+        return status.account.display_name;
+    } else if (status.account.acct) {
+        return status.account.acct;
+    } else {
+        return 'UNKNOWN';
+    }
+}
+
+function statusToItem(status: Status, instance_url: string, access_token: string): Promise<FeedItem> {
+    return new Promise(async (resolve, reject) => {
+        let content = status.content;
+        let actualStatus = status;
+
+        // Reblog check
+        if (status.reblog) {
+            actualStatus = status.reblog;
+            const name = getName(actualStatus);
+            content = `<p>RT ${name}</p>${actualStatus.content}`;
+        }
+
+        // Spoiler check
+        if (actualStatus.spoiler_text) {
+            content = `<p><strong>Spoiler: ${actualStatus.spoiler_text}</strong></p>${content}`;
+        }
+
+        // Reply check
+        if (actualStatus.in_reply_to_id) {
+            const repliedStatus = await getStatus(instance_url, access_token, actualStatus.in_reply_to_id);
+            const name = getName(repliedStatus);
+            if (repliedStatus.account) {
+                content = `<p>Reply to ${name}:</p>${content}`;
+            }
+        }
+
+        // Add media attachments
+        if (actualStatus.media_attachments && actualStatus.media_attachments.length > 0) {
+            for (const media of actualStatus.media_attachments) {
+                content += '<br>';
+                switch (media.type) {
+                    case 'image':
+                        content += `<img src="${media.url}" alt="Image attachment">`;
+                        break;
+                    case 'video':
+                        content += `<video src="${media.url}" controls>Video attachment</video>`;
+                        break;
+                    case 'gifv':
+                        content += `<video src="${media.url}" autoplay loop muted playsinline>Video attachment</video>`;
+                        break;
+                    case 'audio':
+                        content += `<audio src="${media.url}" controls>Audio attachment</audio>`;
+                        break;
+                }
+            }
+        }
+
+        resolve({
+            title: getName(status),
+            id: status.id,
+            link: status.url,
+            content: content,
+            date: new Date(status.created_at),
+        });
+    });
+}
